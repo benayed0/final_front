752d832914de6325a52efc609a4fad23e9d02450
Aziz <mohamed-aziz.ben-ayed@epitech.eu>
2024-07-17 20:45:49 +0200

    trying changes

--- src/app/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://backend-brown-gamma.vercel.app',
-};
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,5 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000',
-};
+// export const environment = {
+//   production: true,
+//   API_URL: 'https://backend-brown-gamma.vercel.app',
+// };
+export const environment = {};
